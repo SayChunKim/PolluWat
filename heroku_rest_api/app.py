defd1ea4d52a638784f92d76cd9ecd260ba7e44d
kklee998 <kahkinlee998@gmail.com>
2018-10-01 17:10:52 +0800

    Fixed API, added .gitignore

--- heroku_rest_api/app.py
+++ heroku_rest_api/app.py
@@ -1,21 +1,17 @@
-import os
-from flask import Flask, jsonify
+from flask import Flask
 import run
 
 
 app = Flask(__name__)
 
 
-result = run.output
-
-
 @app.route('/ml/api/v1.0/output', methods=['GET'])
 def get_output():
 
-    return jsonify(result)
+    output = run.model()
+    # print(output)
+    return output
 
 
 if __name__ == '__main__':
-    # Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0')
